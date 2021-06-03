9ccc909372a3db59db687f7c9a8458d0034445da
Abhi Singh <abhi.s@browserstack.com>
2021-06-03 20:42:32 +0530

    Updated test structure

---
+++ __tests__/util/action.ts
@@ -0,0 +1,32 @@
+export function switchToPaypalFrame(): void {
+  const frame = $("div#paypal-button-container iframe");
+  frame.waitForDisplayed();
+  browser.switchToFrame(frame);
+}
+
+export function clickPaypalButton(): void {
+  const paypalButton = $('[data-funding-source="paypal"]');
+  paypalButton.waitForDisplayed();
+  paypalButton.click();
+}
+
+export function enterPhoneNumber(phoneNumber: string): void {
+  const phoneTextField = $("#email");
+  phoneTextField.waitForDisplayed();
+  phoneTextField.waitForClickable({ timeout: 5000 });
+  browser.pause(5000);
+  phoneTextField.setValue(phoneNumber);
+}
+
+export function clickNextButton(): void {
+  const nextButton = $("#btnNext");
+  nextButton.waitForClickable({ timeout: 5000 });
+  nextButton.click();
+}
+
+export function getWarningText(): string {
+  const notificationWarning = $("#content > div.notifications > p");
+  notificationWarning.waitForDisplayed();
+  const text = notificationWarning.getText();
+  return text;
+}
--- __tests__/util/common.ts
+++ __tests__/util/common.ts
@@ -1,4 +1,22 @@
+import {
+  clickNextButton,
+  clickPaypalButton,
+  enterPhoneNumber,
+  switchToPaypalFrame,
+} from "./action";
+
 export function switchWindow(): void {
   const windows: string[] = browser.getWindowHandles();
   browser.switchToWindow(windows[1]);
+}
+
+export function checkoutWithPaypal(): void {
+  switchToPaypalFrame();
+  clickPaypalButton();
+  browser.switchToParentFrame();
+}
+
+export function loginWithPhoneNumber(phoneNumber: string): void {
+  enterPhoneNumber(phoneNumber);
+  clickNextButton();
 }
